ccf235449006f1c9c2ac8e125c0eb4385b1db156
zhengx55 <williamzhxin@gmail.com>
2023-09-13 10:15:43 +0800

    first commit

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,20 @@
+import express, { Response, Request } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.get('/api/users/signup', [
+    body('email').isEmail().withMessage('Email must be valid'), body('password').trim().isLength({ min: 4, max: 20 }).withMessage('password must be at least 4 and at least 20 characters')
+], (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array())
+    }
+    const { email, password } = req.body;
+    console.log('Creating a User...')
+    res.send({ email: email, password: password })
+
+
+})
+
+export { router as signupRouter };
