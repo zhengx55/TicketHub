ad47ae06eb439f8e1882524cf3445e4626bbe5d9
zhengx55 <williamzhxin@gmail.com>
2023-09-13 14:34:26 +0800

    error-handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,16 @@
 import {
     Request, Response, NextFunction
 } from "express"
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => { 
-    
+import { RequestValidationError } from "../errors/request-validation-error"
+import { DatabaseConnectionError } from "../errors/database-connection-error"
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof RequestValidationError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
+    if (err instanceof DatabaseConnectionError) {
+        return res.status(err.statusCode).send({
+            errors: err.serializeErrors()
+        })
+    }
+
 }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,17 +1,20 @@
 import express, { Response, Request } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
-router.get('/api/users/signup', [
+router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'), body('password').trim().isLength({ min: 4, max: 20 }).withMessage('password must be at least 4 and at least 20 characters')
 ], (req: Request, res: Response) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        throw new Error('Invalid email or password')
+        throw new RequestValidationError(errors.array())
     }
     const { email, password } = req.body;
     console.log('Creating a User...')
+    // throw new DatabaseConnectionError();
     res.send({ email: email, password: password })
 
 
