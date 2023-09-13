0773edb752f557d00a637bea80f30c1d5aa17998
zhengx55 <williamzhxin@gmail.com>
2023-09-13 10:22:29 +0800

    remove node_modules

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -11,8 +12,8 @@ app.use(json());
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signOutRouter);
-app.use(signupRouter)
-
+app.use(signupRouter);
+app.use(errorHandler);
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -8,7 +8,7 @@ router.get('/api/users/signup', [
 ], (req: Request, res: Response) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+        throw new Error('Invalid email or password')
     }
     const { email, password } = req.body;
     console.log('Creating a User...')
