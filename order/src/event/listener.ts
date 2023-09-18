6f87661981f2ff18ab41145c21bd1108c896e406
zhengx55 <williamzhxin@gmail.com>
2023-09-18 22:54:17 +0800

    order listener setup

--- order/src/event/listener.ts
+++ order/src/event/listener.ts
@@ -1,11 +1,40 @@
 import { Message } from "node-nats-streaming";
-import { TicketCreatedEvent } from '../../../common/build/interface/nats-event';
-import { Listener, Subjects } from "@zhengx-test/tickethub-common";
+import { TicketCreatedEvent } from "../../../common/build/interface/nats-event";
+import {
+  Listener,
+  NotFoundError,
+  Subjects,
+  TicketUpdatedEvent,
+} from "@zhengx-test/tickethub-common";
+import { Ticket } from "../model/ticket";
 
-export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
-    queueGroupName: string = 'orders-service';
-    onMessage(data: TicketCreatedEvent['data'], msg: Message){
-    
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName: string = "orders-service";
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = Ticket.build({
+      id,
+      title,
+      price,
+    });
+    await ticket.save();
+    msg.ack();
+  }
+}
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName: string = "orders-service";
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+    if(!ticket){
+        throw new NotFoundError();
     }
+    const {title, price} = data;
+    ticket.set({
+        title, price
+    })
+    await ticket.save();
+  }
 }
--- order/src/index.ts
+++ order/src/index.ts
@@ -2,6 +2,7 @@ import "express-async-errors";
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { TicketCreatedListener, TicketUpdatedListener } from "./event/listener";
 
 const bootstrap = async () => {
   if (!process.env.JWT_KEY) {
@@ -30,7 +31,8 @@ const bootstrap = async () => {
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
     await mongoose.connect(process.env.MONGO_URI);
     console.log("mongod connection established");
   } catch (error) {}
